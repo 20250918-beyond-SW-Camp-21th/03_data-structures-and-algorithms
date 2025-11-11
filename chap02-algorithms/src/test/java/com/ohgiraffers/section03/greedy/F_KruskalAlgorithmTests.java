228c5416e026317538e9c916b891335353e2471c
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-11 09:43:19 +0900

    commit

--- chap02-algorithms/src/test/java/com/ohgiraffers/section03/greedy/F_KruskalAlgorithmTests.java
+++ chap02-algorithms/src/test/java/com/ohgiraffers/section03/greedy/F_KruskalAlgorithmTests.java
@@ -14,6 +14,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+/*
+* 백준(1197)
+* https://www.acmicpc.net/problem/1197
+* */
 class F_KruskalAlgorithmTests {
 
     static String input1, input2;
